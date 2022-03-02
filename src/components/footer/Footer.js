7609fc1f757b7554ba55b2877e30dca1035274f2
mkrndmb <mkrndmb@gmail.com>
2022-03-02 19:18:27 +0530

    changed footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export function Footer(params) {
     return(
         <div className='footer'>
-            Thanks to <a href='https://brittanychiang.com/' target="_blank" rel="noopener noreferrer" className="footerAtag"> Brittany Chian </a> for design ref !! 
+            Thanks to <a href='https://brittanychiang.com/' target="_blank" className="footerAtag"> Brittany Chian </a> for design ref !! 
         </div>
     )
 };
